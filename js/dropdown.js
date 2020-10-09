8e8e7eccccbdbaad7a3a7a4c305c77e7969e6560
Wooseok <wooseok@kudos.co.nz>
2020-10-09 19:33:04 +1300

    Back to old

---
+++ js/dropdown.js
@@ -0,0 +1,20 @@
+function dropProduct() {
+    document.getElementById("products").classList.toggle("show");
+}
+
+function dropFabric() {
+    document.getElementById("fabric").classList.toggle("show");
+}
+
+window.addEventListener = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+         var dropdowns = document.getElementsByClassName("dropdown-content");
+         var i;
+         for (i = 0; i < dropdowns.length; i++) {
+              var openDropdown = dropdowns[i];
+              if (openDropdown.classList.contains('show')) {
+                   openDropdown.classList.remove('show');
+              }
+         }
+    }
+}
